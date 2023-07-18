0cc3d97994be82f99ad0f31158f78fe49eaa9aaa
alesrin <alesrin@icloud.com>
2023-07-18 11:16:33 +0200

    CRUD local, explicación service & interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { DataListComponent } from './data-list/data-list.component';
+import { DataFormComponent } from './data-form/data-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
